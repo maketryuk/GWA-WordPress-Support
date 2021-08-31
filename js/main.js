97b30a831672807abe0f3ef4149a1dae551cc7a1
Nikita <81490302+maketryuk@users.noreply.github.com>
2021-08-31 16:04:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+AOS.init({
+  once: true,
+});
+
+$(function () {
+  $(".link__more").click(function (e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(".packages h2").offset().top
+    }, 800);
+  });
+
+  $('.accordion__title').click(function () {
+    $(".accordion__text").not($(this).next()).slideUp(300);
+    $(this).next().slideToggle(300);
+    $(".accordion__item").not($(this).closest(".accordion__item")).removeClass("active");
+    $(this).closest(".accordion__item").toggleClass("active");
+  });
+});
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+  $('.contact__btn').text('Contact Us');
+} else {
+  $('.contact__btn').text('Choose a package now');
+};
